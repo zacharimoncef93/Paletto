d5c96038278bdd93ffd0b5ae96d10301410e287f
zacharimoncef93 <zhafid16@gmail.com>
2015-11-12 13:09:17 +0100

    message bienvenu

--- src/Engine.js
+++ src/Engine.js
@@ -3,7 +3,28 @@
 var Engine = function () {
 
 // private attributes and methods
-alert("hello");
-    alert("ok");
+    var init=function(){
+
+        board = new Array(6);
+        for(i=0; i<6; i++){
+            board[i]=new Array(6);
+            for(j=0;j<6;j++){
+                board[i][j]="vide";
+            }
+        }
+        board[0][0]="noire";
+        board[0][1]="vert";
+        board[0][2]="blanc";
+        board[0][3]="noire";
+        board[0][4]="noire";
+        board[0][5]="noire";
+        board[0][0]="noire";
+        board[0][0]="noire";
+        board[0][0]="noire";
+        board[0][0]="noire";
+
+
+
+    };
 // public methods
 };
